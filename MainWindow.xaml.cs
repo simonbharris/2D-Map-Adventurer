2ed9ba4a2d4bf39e2b68e10d2a739651f795fafb
Simon Harris <simonbharris@gmail.com>
2018-04-19 23:27:09 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinimapRenderer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Map worldMap;
+        Rectangle[,] mmGrid = new Rectangle[3, 3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            worldMap = new Map();
+            worldMap.GenerateMap();
+            worldMap.MyLocation = new Vector(1, 1);
+            BindRectGrid();
+
+            // Debug (Identify current tile position.)
+            lbldebug.Content = worldMap.myLocation.X + ", " + worldMap.myLocation.Y; 
+
+            RenderSurroundings();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Does all the color
+        private void fillRect(Rectangle rect, Color col)
+        {
+            rect.Fill = new SolidColorBrush(col);
+        }
+
+        private void RenderSurroundings()
+        {
+            int centerX = (int)worldMap.myLocation.X, centerY = (int)worldMap.myLocation.Y;
+
+            // mi, mj = map tiles index // i, j track grid coordinates.
+            // Iterates so that all map tiles are assigned the appropriate color or black of the tile does not exist.
+            for (int i = centerX - 1, mi = 0; i <= centerX + 1; i++, mi++)
+            {
+                for (int j = centerY - 1, mj = 0; j <= centerY + 1; j++, mj++)
+                {
+                    if (j <= 0 || i <= 0 || worldMap.grid[j, i] == null)
+                    {
+                        fillRect(mmGrid[mj, mi], Colors.Black);
+                    }
+                    else
+                    {
+                        fillRect(mmGrid[mj, mi], worldMap.grid[j, i].TileColor);
+                    }
+                }
+            }
+        }
+
+        // Binds GUI rects to mmGrid array.
+        private void BindRectGrid()
+        {
+            mmGrid[0, 0] = rctNW;
+            mmGrid[0, 1] = rctNorth;
+            mmGrid[0, 2] = rctNE;
+            mmGrid[1, 0] = rctWest;
+            mmGrid[1, 1] = rctCenter;
+            mmGrid[1, 2] = rctEast;
+            mmGrid[2, 0] = rctSW;
+            mmGrid[2, 1] = rctSouth;
+            mmGrid[2, 2] = rctSE;
+        }
+
+        // Register GUI clicks.
+        private void rctSouth_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            worldMap.MoveSouth();
+            RenderSurroundings();
+            lbldebug.Content = worldMap.myLocation.X + ", " + worldMap.myLocation.Y;
+        }
+
+        private void rctEast_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            worldMap.MoveEast();
+            RenderSurroundings();
+            lbldebug.Content = worldMap.myLocation.X + ", " + worldMap.myLocation.Y;
+        }
+
+        private void rctNorth_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            worldMap.MoveNorth();
+            RenderSurroundings();
+            lbldebug.Content = worldMap.myLocation.X + ", " + worldMap.myLocation.Y;
+        }
+
+        private void rctWest_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            worldMap.MoveWest();
+            RenderSurroundings();
+            lbldebug.Content = worldMap.myLocation.X + ", " + worldMap.myLocation.Y;
+        }
+    }
+}
---
+++ Map.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MinimapRenderer
+{
+    class Map
+    {
+        const int mapSize = 6;
+        int mapX = mapSize;
+        int mapY = mapSize;
+        public Location[,] grid;
+        public Vector myLocation;
+
+        // Generates a 2d grid and assigns all tiles a generic color.
+        public void GenerateMap()
+        {
+            grid = new Location[mapX + 1, mapY + 1];
+            for(int i = 0; i < mapX; i++)
+            {
+                for(int j = 0; j < mapY; j++)
+                {
+                    grid[i, j] = new Location(i, j);
+                    grid[i, j].TileColor = Colors.LightGreen;
+                }
+            }
+
+            PopulateSpecialLocations();
+        }
+
+        // Generates specially-colored tiles.
+        public void PopulateSpecialLocations()
+        {
+            grid[1, 1].TileColor = Colors.Brown;
+            grid[1, 3].TileColor = Colors.Azure;
+            grid[4, 2].TileColor = Colors.DarkOrchid;
+            grid[3, 4].TileColor = Colors.Blue;
+            grid[4, 5].TileColor = Colors.Red;
+        }
+
+        // Movement methods on cardinals.
+        public void MoveSouth()
+        {
+            if((myLocation.Y < MapSize-1)){
+                myLocation.Y++;
+            }
+        }
+
+        public void MoveEast()
+        {
+            if ((myLocation.X < MapSize - 1))
+            {
+                myLocation.X++;
+            }
+        }
+
+        public void MoveNorth()
+        {
+            if ((myLocation.Y - 1 >= 1))
+            {
+                myLocation.Y--;
+            }
+        }
+
+        public void MoveWest()
+        {
+            if ((myLocation.X - 1 >= 1))
+            {
+                myLocation.X--;
+            }
+        }
+
+        // Accessor / mutations -- Also myLocation should be tracked elsewhere.
+        public int MapSize
+        {
+            get { return mapSize; }
+        }
+        public Vector MyLocation
+        {
+            get { return myLocation; }
+            set { myLocation = value; }
+        }
+
+        public Location GetLocation(int x, int y)
+        {
+            return grid[x, y];
+        }
+
+        public void SetLocation(Location loc, int x, int y)
+        {
+            grid[x, y] = loc;
+        }
+    }
+
+    class Location
+    {
+        string name;
+        int x, y;
+        Color tileColor;
+        public Location(int x, int y, string name)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }
+
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+
+
+        public Color TileColor
+        {
+            get { return tileColor; }
+            set { tileColor = value; }
+        }
+    }
+}
